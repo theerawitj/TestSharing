34f6d0dc6561b28370ff35397ae63172ea91fa5c
theerawitj <theerawit_jampech@hotmail.com>
2021-04-05 20:16:31 +0700

    Test01

--- app/src/main/java/com/testsharing/MainActivity.kt
+++ app/src/main/java/com/testsharing/MainActivity.kt
@@ -2,6 +2,7 @@ package com.testsharing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/testsharing/TestClass.kt
@@ -0,0 +1,4 @@
+package com.testsharing
+
+class TestClass {
+}

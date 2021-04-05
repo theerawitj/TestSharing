204e8e0c65b34a715e92c9e03d02781d677d97be
theerawitj <theerawit_jampech@hotmail.com>
2021-04-05 20:29:36 +0700

    Delete import android.widget.ListView

--- app/src/main/java/com/testsharing/MainActivity.kt
+++ app/src/main/java/com/testsharing/MainActivity.kt
@@ -2,7 +2,6 @@ package com.testsharing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
